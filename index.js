5ded9f2ee2f2341d3a900a6f34bd566c65ca9781
Ashish-1052 <akedari1052@gmail.com>
2025-12-14 15:58:49 +0530

    handled edit for post

--- index.js
+++ index.js
@@ -14,34 +14,39 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("home.ejs", { blogs });
+  res.render("home.ejs", { blogs });
 });
 
 app.get("/blog/:id", (req, res) => {
-  console.log('req', req);
-    selectedBlog = blogs.find(blog => blog.id === req.params.id);
-    res.render("submit-blog.ejs", { selectedBlog });
+  selectedBlog = blogs.find(blog => blog.id === req.params.id);
+  if (selectedBlog) {
+    res.render("submit-blog.ejs", {
+      title: selectedBlog.title,
+      content: selectedBlog.content,
+      id: selectedBlog.id
+    });
+  } else {
+    res.render("submit-blog.ejs");
+  }
 });
 
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
 
 app.get("/submit-blog", (req, res) => {
-    res.render("submit-blog.ejs");
+  res.render("submit-blog.ejs");
 });
 
 app.post("/submit-blog", (req, res) => {
-    console.log(req.body);
-    let blogToPost = req.body;
-    blogs.push(blogToPost);
-    res.redirect("/");
+  let blogToPost = req.body;
+  blogs.push(blogToPost);
+  res.redirect("/");
 });
 
-app.put("/edit-blog", (req, res) => {
-    console.log(req.body);
-    let blogIdToEdit = req.body.id;
-    blogs = blogs.map(blog => blog.id === blogIdToEdit ? req.body : blog);
-    res.redirect("/");
+app.post("/edit-blog", (req, res) => {
+  let blogIdToEdit = req.body.id;
+  blogs = blogs.map(blog => blog.id === blogIdToEdit ? req.body : blog);
+  res.redirect("/");
 });
 
